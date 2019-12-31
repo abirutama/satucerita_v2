dfd7d2615b95ef73b17968013c4922b139f86c25
abirutama <abirutama@gmail.com>
2019-12-31 15:29:05 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Home</title>
+  </head>
+  <body>
+    <div id="parent" class="container" style="max-width: 480px">
+      <section id="head" class="mt-4 mb-4">
+        <div id="welcome-section">
+          <h2><span>Username</span>,</h2>
+          <h2>Selamat Datang!</h2>
+        </div>  
+      </section>
+
+      <section id="body" class="mb-4">
+        <div id="card-section" class="mb-4">
+          <a href="">
+            <img src="https://via.placeholder.com/300x170.png" alt="..." class="img-thumbnail">
+          </a>
+        </div>
+
+        <div>
+          <h4>Penyesuaian</h4>
+          <div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item d-flex justify-content-between align-items-center">
+                <img src="https://via.placeholder.com/50x50.png" alt="" class="rounded-circle">
+                <span class="mr-auto" style="margin-left: 16px">Tambah Stories</span>
+                <i class="fas fa-angle-right"></i>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center">
+                <img src="https://via.placeholder.com/50x50.png" alt="" class="rounded-circle">
+                <span class="mr-auto" style="margin-left: 16px">Pilih Tema</span>
+                <i class="fas fa-angle-right"></i>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center">
+                <img src="https://via.placeholder.com/50x50.png" alt="" class="rounded-circle">
+                <span class="mr-auto" style="margin-left: 16px">Produk Lainnya</span>
+                <i class="fas fa-angle-right"></i>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </section>
+
+      <section id="footer">
+        
+      </section>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://kit.fontawesome.com/5dbbe055c9.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
